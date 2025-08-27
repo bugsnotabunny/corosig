b836acc56f242313b49a2387130dd30ca79bfc8f
Dmitry Malakhov <d.malakhov@yadro.com>
2025-08-27 21:26:56 +0300

    update xmake configs

--- xmake.lua
+++ xmake.lua
@@ -4,29 +4,30 @@ set_languages("c++20")
 set_warnings("all", "error")
 
 if is_mode("debug") then
-    set_symbols("debug")
-    set_optimize("fast")
+    set_policy("build.sanitizer.address", true)
+    add_cxflags("-g -Og")
 else
     set_symbols("hidden")
     set_optimize("fastest")
+    set_policy("build.optimization.lto", true)
     set_strip("debug")
 end
 
-target("sigawait")
-    set_kind("static")
-    add_includedirs("include", {public=true})
-    add_files("src/*.cpp")
-    set_default(true)
+target("corosig")
+set_kind("static")
+add_includedirs("include", { public = true })
+add_files("src/*.cpp")
+set_default(true)
 target_end()
 
-add_requires("catch2", {configs = {main = true, gmock = true}})
+add_requires("catch2", { configs = { main = true, gmock = true } })
 
 for _, file in ipairs(os.files("test/*.cpp")) do
-    local name = "test_".. path.basename(file)
+    local name = "test_" .. path.basename(file)
     target(name)
-        set_kind("binary")
-        add_deps("sigawait")
-        add_packages("catch2")
-        add_files(file)
-        add_tests("default")
+    set_kind("binary")
+    add_deps("corosig")
+    add_packages("catch2")
+    add_files(file)
+    add_tests("default")
 end
