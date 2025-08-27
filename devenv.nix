fcb38bc61ce6ccc4b535343b3c676600768a1bea
Dmitry Malakhov <d.malakhov@yadro.com>
2025-08-27 21:27:16 +0300

    devenv upd

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,8 @@
 # https://devenv.sh/
-
-{ pkgs, lib, config, inputs, ... }:
-{
-  packages = with pkgs; [
-    llvmPackages_latest.clang-tools
-    xmake
-    valgrind
-    gdb
-  ];
+{ pkgs, ... }: {
+  packages = with pkgs; [ llvmPackages_latest.clang-tools xmake valgrind gdb ];
 
   languages.cplusplus.enable = true;
   languages.nix.enable = true;
+  languages.lua.enable = true;
 }
