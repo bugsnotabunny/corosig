700f61be7e391d0a9faab048f90a53e7d8f83dc3
Dmitry Malakhov <d.malakhov@yadro.com>
2025-10-10 02:47:12 +0300

    ubuntu workflow tryout

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,10 @@
 # https://devenv.sh/
 { pkgs, ... }: {
-  packages = with pkgs; [ llvmPackages_latest.clang-tools xmake valgrind gdb ];
+  packages = with pkgs; [
+    llvmPackages_latest.clang-tools
+    xmake
+    gdb
+  ];
 
   languages.cplusplus.enable = true;
   languages.nix.enable = true;
