650374a57821c0c2aaa4cd9f08cc2986280eb9b7
Dmitry Malakhov <d.malakhov@yadro.com>
2026-05-26 23:00:16 +0300

    Add more endianness conversions

--- include/corosig/util/Endianness.hpp
+++ include/corosig/util/Endianness.hpp
@@ -4,19 +4,32 @@
 #include <algorithm>
 #include <array>
 #include <bit>
+#include <climits>
 #include <concepts>
+#include <span>
 #include <type_traits>
 
 namespace corosig {
 
+namespace detail {
+
 template <typename T>
   requires(std::integral<T> || std::is_enum_v<T>)
-constexpr T hton(T value) noexcept {
+// std::byteswap did not make it into c++20
+constexpr T byteswap(T value) noexcept {
+  static_assert(std::has_unique_object_representations_v<T>, "T shall not have padding bits");
+  auto value_representation = std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
+  std::ranges::reverse(value_representation);
+  return std::bit_cast<T>(value_representation);
+}
+
+} // namespace detail
+
+template <typename T>
+  requires(std::integral<T> || std::is_enum_v<T>)
+constexpr T betoh(T value) noexcept {
   if constexpr (std::endian::native == std::endian::little) {
-    static_assert(std::has_unique_object_representations_v<T>, "INT may not have padding bits");
-    auto value_representation = std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
-    std::ranges::reverse(value_representation);
-    return std::bit_cast<T>(value_representation);
+    return detail::byteswap(value);
   } else if (std::endian::native == std::endian::big) {
     return value;
   } else {
@@ -26,8 +39,50 @@ constexpr T hton(T value) noexcept {
 
 template <typename T>
   requires(std::integral<T> || std::is_enum_v<T>)
+constexpr T letoh(T value) noexcept {
+  if constexpr (std::endian::native == std::endian::little) {
+    return value;
+  } else if (std::endian::native == std::endian::big) {
+    return detail::byteswap(value);
+  } else {
+    static_assert(false, "Unsupported byte order");
+  }
+}
+
+template <typename T>
+  requires(std::integral<T> || std::is_enum_v<T>)
+constexpr T htole(T value) noexcept {
+  if constexpr (std::endian::native == std::endian::little) {
+    return value;
+  } else if (std::endian::native == std::endian::big) {
+    return betole(value);
+  } else {
+    static_assert(false, "Unsupported byte order");
+  }
+}
+
+template <typename T>
+  requires(std::integral<T> || std::is_enum_v<T>)
+constexpr T htobe(T value) noexcept {
+  if constexpr (std::endian::native == std::endian::little) {
+    return detail::byteswap(value);
+  } else if (std::endian::native == std::endian::big) {
+    return value;
+  } else {
+    static_assert(false, "Unsupported byte order");
+  }
+}
+
+template <typename T>
+  requires(std::integral<T> || std::is_enum_v<T>)
+constexpr T hton(T value) noexcept {
+  return htobe(value);
+}
+
+template <typename T>
+  requires(std::integral<T> || std::is_enum_v<T>)
 constexpr T ntoh(T value) noexcept {
-  return hton(value);
+  return betoh(value);
 }
 
 } // namespace corosig
