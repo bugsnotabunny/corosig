a5dc54807d642574c0576468badaa996e78be3c2
Dmitry Malakhov <d.malakhov@yadro.com>
2026-06-23 17:59:55 +0300

    Add xmake package

---
+++ packages/c/corosig/xmake.lua
@@ -0,0 +1,18 @@
+package("corosig")
+    set_description("CoroSig - A signal handling library for C++20")
+    set_homepage("https://github.com/bugsnotabunny/corosig")
+    set_license("MIT")
+
+    add_urls("https://github.com/bugsnotabunny/corosig.git")
+
+    add_versions("experimental", "master")
+
+    add_deps("boost 1.86.0", { configs = { filesystem = false } })
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package, { tests = false, examples = false })
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxincludes("corosig/Sighandler.hpp", {configs = {languages = "cxx20"}}))
+    end)
