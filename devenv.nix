8a355ccbc3617e7c470e8a9aaec677ae3ed36e10
Dmitry Malakhov <d.malakhov@yadro.com>
2026-07-13 14:06:50 +0300

    Upd devenv

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,9 @@
 # https://devenv.sh/
 { pkgs, ... }: {
   packages = with pkgs; [
-    llvmPackages_21.clang-tools
-    llvmPackages_21.bintools
-    llvmPackages_19.libstdcxxClang
+    llvmPackages_latest.clang-tools
+    llvmPackages_latest.bintools
+    llvmPackages_latest.libstdcxxClang
     doxygen
     xmake
     gdb
