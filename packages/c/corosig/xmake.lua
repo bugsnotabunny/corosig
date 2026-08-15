fbebc8400789a8807f463a1a38d17644e072cc70
Dmitry Malakhov <malakhov.malakhit@gmail.com>
2026-08-15 23:54:30 +0300

    new version leets gooooo

--- packages/c/corosig/xmake.lua
+++ packages/c/corosig/xmake.lua
@@ -8,6 +8,7 @@ package("corosig")
     add_versions("experimental", "master")
     add_versions("v0.1.0", "v0.1.0")
     add_versions("v0.1.1", "v0.1.1")
+    add_versions("v0.1.2", "v0.1.2")
 
 
     add_deps("boost", { configs = { filesystem = false } })
--- test/cases/io/TcpSocket.cpp
+++ test/cases/io/TcpSocket.cpp
@@ -11,7 +11,6 @@
 #include <netinet/in.h>
 #include <span>
 #include <sys/socket.h>
-#include <unistd.h>
 
 namespace {
 
