8fdd11a37bd581f20f7afb52f5f2366ea5ff9c60
Dmitry Malakhov <malakhov.malakhit@gmail.com>
2026-08-15 19:01:54 +0300

    Split devenv into multiple profiles

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,60 @@
 # https://devenv.sh/
-{ pkgs, ... }: {
-  packages = with pkgs; [
-    llvmPackages_latest.clang-tools
-    llvmPackages_latest.bintools
-    llvmPackages_latest.libstdcxxClang
-    gcc_latest
-    doxygen
-    xmake
-    gdb
-  ];
+{
+  profiles = {
+    noCaches.module = {
+      cachix.enable = false;
+    };
+
+    caches.module = {
+      cachix = {
+        enable = true;
+        pull = [
+          "nix-community"
+          "devenv"
+          "numtide"
+        ];
+      };
+    };
+
+    gcc.module = { pkgs, ... }: {
+      packages = with pkgs; [
+        llvmPackages_latest.bintools
+        gccStdenv
+        xmake
+      ];
+    };
+
+    clang.module = { pkgs, ... }: {
+      packages = with pkgs; [
+        llvmPackages_latest.bintools
+        llvmPackages_latest.libcxxClang
+        xmake
+      ];
+    };
+
+    codestyle.module = { pkgs, ... }: {
+      packages = with pkgs; [
+        llvmPackages_latest.clang-tools
+        doxygen
+        xmake
+      ];
+    };
+
+    debuggers.module = { pkgs, ... }: {
+      packages = with pkgs; [
+        gdb
+        llvmPackages_latest.lldb
+      ];
+    };
+
+    dev = {
+      extends = [
+        "caches"
+        "gcc"
+        "clang"
+        "codestyle"
+        "debuggers"
+      ];
+    };
+  };
 }
